62702b917d1d86ab059b5fdb793e11a6bdf3481f
tusharparimi <tushar.parimi@gmail.com>
2025-10-31 10:07:41 +0530

    pset1 problem 3a solved

--- pset1_starter_code/question3/question3a.js
+++ pset1_starter_code/question3/question3a.js
@@ -4,6 +4,78 @@ let lang = [
     // A few language elements are provided for you.
     // YOUR CODE HERE.
     {
+        name: "ternary",
+        kind: "fun",
+        type: "bool->float->float->float",
+        imp: function (cond, a, b) {
+            return cond ? a : b;
+        }
+    },
+    {
+        name: "less",
+        kind: "fun",
+        type: "float->float->bool",
+        imp: function (x, y) {
+            return x < y;
+        }
+    },
+    {
+        name: "add",
+        kind: "fun",
+        type: "float->float->float",
+        imp: function (x, y) {
+            return x + y;
+        }
+    },
+    {
+        name: "sub",
+        kind: "fun",
+        type: "float->float->float",
+        imp: function (x, y) {
+            return x - y;
+        }
+    },
+    {
+        name: "mul",
+        kind: "fun",
+        type: "float->float->float",
+        imp: function (x, y) {
+            return x * y;
+        }
+    },
+    {
+        name: "div",
+        kind: "fun",
+        type: "float->float->float",
+        imp: function (x, y) {
+            return x / y;
+        }
+    },
+    {
+        name: "exp",
+        kind: "fun",
+        type: "float->float->float",
+        imp: function (x, y) {
+            return x ** y;
+        }
+    },
+    {
+        name: "tan",
+        kind: "fun",
+        type: "float->float",
+        imp: function (x) {
+            return Math.tan(x);
+        }
+    },
+    {
+        name: "cos",
+        kind: "fun",
+        type: "float->float",
+        imp: function (x) {
+            return Math.cos(x);
+        }
+    },
+    {
         name: "sin",
         kind: "fun",
         type: "float->float",
